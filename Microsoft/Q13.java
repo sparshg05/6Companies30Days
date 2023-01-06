aa73021f68a2d10845b99359effb973c2054ec02
Sparsh Goyal <sparshg05@gmail.com>
2023-01-06 19:31:04 +0530

    Airplane Seat Assignment Probability

---
+++ Microsoft/Q13.java
@@ -0,0 +1,11 @@
+package Microsoft;
+
+import java.util.*;
+
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        double x=1;
+        if(n==1) return x;
+        else return x/2;
+    }
+}
